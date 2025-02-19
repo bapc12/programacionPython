16255f0d2ff90fe017511363bd624bd9a1853ca4
bapc12 <baruchandre3@gmail.com>
2025-02-19 10:52:04 -0600

    Add files via upload

---
+++ teoria/clasesYMetodos/Cliente.py
@@ -0,0 +1,11 @@
+class Cliente:
+    def __init__(self, nombre, direccion, edad):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.edad = edad
+
+    def imprimirDetalles(self):
+        print("Detalles del Cliente:")
+        print("Nombre:", self.nombre)
+        print("Dirección:", self.direccion)
+        print("Edad:", self.edad)
---
+++ teoria/clasesYMetodos/MenuBanco.py
@@ -0,0 +1,5 @@
+class MenuBanco:
+    def __init__(self, mensajeBienvenida):
+        self.mensajeBienvenida = mensajeBienvenida
+    def bienvenida(self):
+        print(self.mensajeBienvenida)
